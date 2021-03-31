d95e09a997da043755d647a50bad95fca1740285
AndreM27 <63149136+AndreM27@users.noreply.github.com>
2021-03-31 12:14:40 +0200

    Add files via upload

---
+++ Health-kiosk/kiosk_fe/src/components/Pages/Measurements.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../../app.css'
+import Results from '../Results'
+
+
+function Measurements() {
+    return (
+        <>
+        
+            <Results />
+            
+        </>
+    );
+}
+
+export default Measurements;
---
+++ Health-kiosk/kiosk_fe/src/components/Pages/SignUp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../app.css'
+import CreateAcc from '../CreateAcc'
+function SignUp() {
+    return (
+        <>
+            <CreateAcc />
+        </>
+    )
+}
+
+export default SignUp;
